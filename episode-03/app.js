90681854289274e22792e0dd3f4fceadd35539c8
Shiv Singh <shivsinghcse@gmail.com>
2025-10-02 08:13:17 +0530

    feat: day-03 complete

---
+++ episode-03/app.js
@@ -0,0 +1,11 @@
+var name = "Namaste NodeJS";
+
+var a = 10;
+var b = 20;
+
+// console.log(name);
+// console.log(a+b);
+
+// console.log(global);
+console.log(this); // empty object
+console.log(globalThis === global);
